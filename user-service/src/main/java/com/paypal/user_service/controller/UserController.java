2c3f7920b862853531bbd86fd32e5207264e6a51
InDepthCode <amrut.work@gmail.com>
2025-11-03 16:05:27 +0530

    implement user-service

---
+++ user-service/src/main/java/com/paypal/user_service/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.paypal.user_service.controller;
+
+import com.paypal.user_service.entity.User;
+import com.paypal.user_service.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+}
---
+++ user-service/src/main/java/com/paypal/user_service/entity/User.java
@@ -0,0 +1,27 @@
+package com.paypal.user_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "app_user" )
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
